6465b5dd0c8166856a6c81458a32193eb45c8242
Cathal Donohoe <cdonohoe1374@gmail.com>
2020-12-06 19:25:11 +0000

    Switched to spring boot with login working(BE)

---
+++ FYPproject/src/test/java/gmit/ie/sw/FYPproject/FyPprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package gmit.ie.sw.FYPproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FyPprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
