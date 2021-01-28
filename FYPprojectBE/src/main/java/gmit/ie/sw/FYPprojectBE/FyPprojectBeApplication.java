757122a09ece0d9242f379c8b21a401a754f07bd
Cathal Donohoe <cdonohoe1374@gmail.com>
2021-01-28 11:26:26 +0000

    Adding back and front end together

---
+++ FYPprojectBE/src/main/java/gmit/ie/sw/FYPprojectBE/FyPprojectBeApplication.java
@@ -0,0 +1,13 @@
+package gmit.ie.sw.FYPprojectBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FyPprojectBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FyPprojectBeApplication.class, args);
+	}
+
+}
---
+++ FYPprojectBE/src/test/java/gmit/ie/sw/FYPprojectBE/FyPprojectBeApplicationTests.java
@@ -0,0 +1,13 @@
+package gmit.ie.sw.FYPprojectBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FyPprojectBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
