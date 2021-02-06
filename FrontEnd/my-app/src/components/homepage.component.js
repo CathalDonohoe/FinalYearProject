84dcd26ef53f816d09fd728994b8dc4df93ed596
NathanGarrihy <37175360+NathanGarrihy@users.noreply.github.com>
2021-02-06 00:03:07 +0000

    State change when logged in working

--- FrontEnd/my-app/src/components/homepage.component.js
+++ FrontEnd/my-app/src/components/homepage.component.js
@@ -44,7 +44,7 @@ export default class Homepage extends Component {
       <html>
         <head>
         <title>HomePage</title>
-          <meta charet="utf-8" />
+          <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
           <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
--- FrontEnd/my-app/src/components/navagation.componant.js
+++ FrontEnd/my-app/src/components/navagation.componant.js
@@ -6,8 +6,21 @@ export default class Navagation extends Component {
         localStorage.getItem('user');
         console.log('inside navigation componentdidmount ' + localStorage.getItem('user'));
     }
+    
+    logout() {
+        console.log("Logout done!!!");
+        localStorage.clear();
+        window.location.reload(false);
+    }
 
     render() {
+        let isLoggedIn = false;
+        if(localStorage.getItem('token') && localStorage.getItem('user') != null) {
+            isLoggedIn = true;
+        }
+
+        let myUser = localStorage.getItem('user');
+        
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
                 <div className="container">
@@ -22,12 +35,27 @@ export default class Navagation extends Component {
                                 <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
                                 <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                             </form>
-                            <li className="nav-item">
+                            {  isLoggedIn && (
+                                <li className="nav-item">
+                                <Link className="nav-link" to={"/homepage"}>{myUser}</Link>
+                                </li>
+                            )} 
+                             {  !isLoggedIn && (
+                                <li className="nav-item">
                                 <Link className="nav-link" to={"/login"}>Login</Link>
+                                </li>
+                            )} 
+                             {  isLoggedIn && (
+                                <li className="nav-item">
+                                <Link className="nav-link" onClick={() => this.logout()}>{'Logout'}</Link>
                             </li>
-                            <li className="nav-item">
+                            )} 
+                             {  !isLoggedIn && (
+                                <li className="nav-item">
                                 <Link className="nav-link" to={"/signup"}>Sign up</Link>
                             </li>
+                            )} 
+                            
                         </ul>
                     </div>
                 </div>
