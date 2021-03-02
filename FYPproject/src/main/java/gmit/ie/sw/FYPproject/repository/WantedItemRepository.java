a49b08a8396921c217afc2411d47bead9a8e5a3a
Cathal Donohoe <cdonohoe1374@gmail.com>
2021-03-02 15:28:24 +0000

    WantedItemRepository added

---
+++ FYPproject/src/main/java/gmit/ie/sw/FYPproject/repository/WantedItemRepository.java
@@ -0,0 +1,12 @@
+package gmit.ie.sw.FYPproject.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import gmit.ie.sw.FYPproject.models.WantedItem;
+
+public interface WantedItemRepository extends MongoRepository<WantedItem, String> {
+    List<WantedItem> findByTitleContaining(String title);
+    List<WantedItem> findByPublished(boolean published);
+}
